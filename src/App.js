c72df9214a21599cfbfed4c1b23b9b25a3d687c0
DevSteg <j.l.stephens91@gmail.com>
2022-02-18 14:31:59 +0000

    Add and style landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles/_app.scss";
 import { Nav } from "./components/Nav";
+import { Landing } from "./components/Landing";
 
 function App() {
 	return (
@@ -7,7 +8,9 @@ function App() {
 			<header className="App-header">
 				<Nav />
 			</header>
-			<main className="App-main"></main>
+			<main className="App-main">
+				<Landing />
+			</main>
 		</div>
 	);
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/_landing.scss";
+
+export const Landing = () => {
+	return (
+		<section>
+			<article className="green-roll" id="home">
+				<div className="greet-div">
+					<h1 className="hi-title">Hi,</h1>
+					<h2 className="intro-title">I'm James Stephens</h2>
+					<h3 className="info-title">A Web Developer</h3>
+					<p className="intro-para">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+						pariatur.
+					</p>
+				</div>
+			</article>
+			<aside className="img-aside">
+				<img
+					src={process.env.PUBLIC_URL + "/img/logo.png"}
+					alt="James Stephens"
+				/>
+			</aside>
+		</section>
+	);
+};
